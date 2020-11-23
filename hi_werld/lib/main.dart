b39b3a90ac4adea3351ec668c2c4005fd15a7961
thaju <wondringpsycho@gmail.com>
2020-11-23 21:30:39 +0530

    first commit

---
+++ hi_werld/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MaterialApp(
+    title: "mallu dedelopers",
+    home: Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("malludevelopers"),
+        ),
+      ),
+    ),
+  )
+  );
+}
